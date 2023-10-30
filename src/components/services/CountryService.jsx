a918a35e26aa63f309225b55ebc529fb2d75e56a
“tiagoluis12” <tiago7164@hotmail.com>
2023-10-30 18:32:27 +0000

    fix bugs

--- src/components/services/CountryService.jsx
+++ src/components/services/CountryService.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://backend-country-a4390b041bcf.herokuapp.com/api/countries";
+const API_BASE_URL = "https://country-info-back.onrender.com/api/countries";
 
 // If you want to run using the hosted backend on Render, you have to change the URL in the line above to the following:
 // const API_BASE_URL = "https://country-info-back.onrender.com/api/countries";
